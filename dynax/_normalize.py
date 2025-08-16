a9821c06c0ed8971b9c56694c8fd8113b2a3d784
Fabian Roth <roth@cps.tu-darmstadt.de>
2025-08-17 00:58:41 +0200

    Added docstring for normalize_std

--- dynax/_normalize.py
+++ dynax/_normalize.py
@@ -2,68 +2,130 @@
 from typing import Literal, cast
 
 from jax import numpy as jnp
-from jaxtyping import Array, Float, Scalar
+from jaxtyping import Array, Float
 from scipy.optimize import OptimizeResult, bracket, minimize_scalar
 
-# TODO: Add tests (see below)
+# TODO: Add tests!
+# TODO: Finalize the edgecases: y and a given v not.
 # TODO: Add more userfriendly normalization
-# TODO: Finalize docstring
+
+type Scalar = Float[Array, "1"]
 
 
 def normalize_std(
-    std: Float[Array, "n"],
-    std_t: Float[Array, "n"] | None = None,
-    std_tt: Float[Array, "n"] | None = None,
+    std_y: Float[Array, "n"],
+    std_v: Float[Array, "n"] | None = None,
+    std_a: Float[Array, "n"] | None = None,
+    w_y: float = 1.0,
+    w_v: float = 1.0,
+    w_a: float = 1.0,
     tol: float = 1e-6,
     maxiter: int = 50,
     verbosity: Literal[0, 1, 2, 3] = 1,
 ) -> tuple[Float[Array, "n"], Scalar]:
-    r"""Compute scaling factors for consistent signal normalization.
+    r"""Compute normalization factors for signals and their derivatives while preserving derivative consistency.
 
-    Given the standard deviations of a signal $y(t) \in\mathbb{R}^n$ and (optionally)
-    its first and second derivatives $\partial_t y(t), \partial_{tt} y$, computes
-    the scaling factors $\alpha\in\mathbb{R}^n$ and $\tau\in\mathbb{R}$ such that the rescaled signals
-    $ \tilde t * \tau = t,$
-    $    \quad   \tilde y_i = \alpha_i\y_i,   \quad   \partial_{\tilde t} \tilde{y}_i = \alpha_i\partial_t y_i,   \quad   \partial_{\tilde t\tilde t} \tilde{y}_i = \alpha_i\partial_{tt} y_i $
-    minimize ...
+    Consider a trajectory $y(t) \in \mathbb{R}^n$ together with its derivatives
+        $v(t) = \partial_t \, y(t),\; a(t) = \partial_{tt} \, y(t)$.
+    Normalizing $y, v, a$ independently (e.g. to unit variance) breaks the
+    derivative relationships: the derivative of the normalized trajectory
+    $\partial_t \tilde y(t)$ would no longer match the normalized velocity
+    $\tilde v(t)$.
+
+    To maintain consistency, the same scaling factor $\alpha_i$ must be applied
+    to $y_i, v_i, a_i$. Additionally, we allow a rescaling of time,
+    $\tilde t = \tau t$, to gain more flexibility.
+    With this, the rescaled signals are
+    $$
+        \tilde t = \tau t, \quad
+        \tilde y_i = \alpha_i y_i, \quad
+        \tilde v_i = \tau \alpha_i v_i, \quad
+        \tilde a_i = \tau^2 \alpha_i a_i.
+    $$
+
+    This function finds the scaling factors $\alpha \in \mathbb{R}^n$ and
+    $\tau \in \mathbb{R}$ that make the standard deviations of
+    $\tilde y, \tilde v$ and  $\tilde a$
+    as close as possible to one, while preserving derivative consistency:
+    $$
+        \partial_{\tilde t} \, \tilde y_i(\tilde t) = \tilde v_i(\tilde t), \quad
+        \partial_{\tilde t \tilde t} \, \tilde y_i(\tilde t) = \tilde a_i(\tilde t).
+    $$
+
+    This is done via the following optimization problem:
+    Given standard deviations $\sigma(y), \sigma(v), \sigma(a)$, minimize
+    $$
+        \sum_i \big[
+            w_y (\sigma(\tilde y_i) - 1)^2 \;+\;
+            w_v (\sigma(\tilde v_i) - 1)^2 \;+\;
+            w_a (\sigma(\tilde a_i) - 1)^2
+        \big],
+    $$
+    where $w_y, w_v, w_a$ control the relative importance of each term.
+
+    Implementation Notes:
+        The problem reduces to a one-dimensional optimization over $\tau$, with
+        $$
+            \alpha_i(\tau) = \frac{N_i(\tau)}{D_i(\tau)}, \quad
+            \tau^\star = \underset{\tau}{\operatorname{argmin}}
+                \sum_i -\frac{N_i(\tau)^2}{D_i(\tau)}.
+        $$
+        This function solves the reformulated problem using `scipy.optimize.minimize`
+        with the BFGS method. Additionally:
+
+        - Optimization is performed over $\log(\tau)$ to enforce $\tau > 0$.
+        - The initial guess $\tau_0$ is computed heuristically from
+        $\sigma(y), \sigma(v), \sigma(a)$.
 
     Args:
-        std: _description_
-        std_t: _description_. Defaults to None.
-        std_tt: _description_. Defaults to None.
-        tol: _description_. Defaults to 1e-6.
-        maxiter: _description_. Defaults to 50.
-        verbosity: _description_. Defaults to 1.
-
-    Raises:
-        ValueError: _description_
+        std_y: Standard deviation of the signal $y(t)$.
+        std_v: Standard deviation of the signal $v(t) = \partial_t y(t)$.
+            Optinoal. Defaults to None.
+        std_a: Standard deviation of the signal $a(t) = \partial_{tt} y(t)$.
+            Optinoal. Defaults to None.
+        w_y: Optimization weight for signal $y(t)$. Defaults to 1.0.
+        w_v: Optimization weight for signal $v(t)$. Defaults to 1.0.
+        w_a: Optimization weight for signal $a(t)$. Defaults to 1.0.
+        tol: Relative error in solution for $\tau$ acceptable for convergence.
+            Defaults to 1e-6.
+        maxiter: Maximum number of optimization iterations to perform.
+            Defaults to 50.
+        verbosity: If non-zero, print messages.
+            `0` : no message printing.
+            `1` : non-convergence notification messages only.
+            `2` : print a message on convergence too.
+            `3` : print iteration results.
+            Defaults to 1.
 
     Returns:
-        _description_
+        Tuple containing $\alpha_i$ and $\tau$.
     """
-    if std_tt is None:
-        # Exact solution available
-        alpha = 1 / std
-        if std_t is None:
+    # Treat the edge cases where an exact solution is available
+    if std_a is None:
+        alpha = 1 / std_y
+        if std_v is None:
             tau = jnp.array(1.0)  # Best arbitrary choice
         else:
-            _temp = alpha * std_t
+            _temp = alpha * std_v
             tau = jnp.sum(_temp) / jnp.sum(_temp**2)
         return alpha, tau
 
-    if std_t is None:
-        raise ValueError("std_t cannot be None if std_tt is provided.")
+    if std_v is None:
+        raise ValueError("std_v cannot be None if std_a is provided.")
 
     # Numerically solve the optimization problem
-    std_sq = std**2
-    std_t_sq = std_t**2
-    std_tt_sq = std_tt**2
+    c_y = w_y * std_y
+    c_v = w_v * std_v
+    c_a = w_a * std_a
+    c_y_sq = c_y * std_y
+    c_v_sq = c_v * std_v
+    c_a_sq = c_a * std_a
 
     def numerator(tau: Float[Array, "1"]) -> Float[Array, "n"]:
-        return std + std_t * tau + std_tt * tau**2
+        return c_y + c_v * tau + c_a * tau**2
 
     def denominator(tau: Float[Array, "1"]) -> Float[Array, "n"]:
-        return std_sq + std_t_sq * tau**2 + std_tt_sq * tau**4
+        return c_y_sq + c_v_sq * tau**2 + c_a_sq * tau**4
 
     def fun(log_tau: Float[Array, "1"]):
         tau = jnp.exp(log_tau)
@@ -72,9 +134,9 @@ def normalize_std(
 
     # Initial guess
     eps = 1e-10
-    alpha0 = 1.0 / (std + eps)
-    tau1 = jnp.mean(1.0 / (alpha0 * (std_t + eps)))
-    tau2 = jnp.sqrt(jnp.mean(1.0 / (alpha0 * (std_tt + eps))))
+    alpha0 = 1.0 / (std_y + eps)
+    tau1 = jnp.mean(1.0 / (alpha0 * (std_v + eps)))
+    tau2 = jnp.sqrt(jnp.mean(1.0 / (alpha0 * (std_a + eps))))
     log_tau_init = 0.5 * float(jnp.log(jnp.abs(tau1) * jnp.abs(tau2)))
 
     # Find starting bracket
