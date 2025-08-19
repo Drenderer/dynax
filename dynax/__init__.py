7e1cd004b404f98efaf9139a222939fec7232a63
Fabian Roth <roth@cps.tu-darmstadt.de>
2025-08-19 11:45:56 +0200

    Solved import issue in __init__.py

--- dynax/__init__.py
+++ dynax/__init__.py
@@ -2,7 +2,9 @@ from ._isphs import ISPHS as ISPHS
 from ._lyapunov_function import (
     ConvexLyapunov as ConvexLyapunov,
 )
-from ._normalize import normalization_coefficients
+from ._normalize import (
+    normalization_coefficients as normalization_coefficients,
+)
 from ._odesolver import ODESolver as ODESolver
 from ._singals import aprbs as aprbs
 from ._singals import (
