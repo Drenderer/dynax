de6fd3c5f6fe504f8072ea304d9dcf993f57c24a
Fabian Roth <roth@cps.tu-darmstadt.de>
2026-01-14 18:36:13 +0100

    Added neural ODE implementation.

--- dynax/__init__.py
+++ dynax/__init__.py
@@ -4,11 +4,13 @@ from ._isphs import ISPHS as ISPHS
 from ._lyapunov_function import (
     ConvexLyapunov as ConvexLyapunov,
 )
+from ._node import NeuralODE as NeuralODE
 from ._normalize import (
     normalization_coefficients as normalization_coefficients,
 )
 from ._odesolver import ODESolver as ODESolver
-from ._pod import PODLatentSpace, get_svd
+from ._pod import PODLatentSpace as PODLatentSpace
+from ._pod import get_svd as get_svd
 from ._singals import aprbs as aprbs
 from ._singals import (
     bandlimited_noise as bandlimited_noise,
