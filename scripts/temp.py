bc02e6283d4ffea997e64aef59ea46029f05c5d3
Fabian Roth <roth@cps.tu-darmstadt.de>
2026-06-02 17:19:06 +0200

    Modified temporary script to test the new args call interface.

--- scripts/temp.py
+++ scripts/temp.py
@@ -2,9 +2,49 @@
 import equinox as eqx
 import jax
 import jax.numpy as jnp
+import klax
 import matplotlib.pyplot as plt
 from jax import random as jr
 from jaxtyping import Array
-from klax import fit
 
-from dynax import CALMLayer, PointArgFunc
+from dynax import (
+    ISOPHS,
+    ConvexNormalization,
+    MatrixWrapper,
+    NoArgsPotential,
+    ODESolver,
+)
+
+# %%
+
+f = klax.nn.PICNN(2, 1, "scalar", [(8, 4), (8, 4)], key=jr.key(0))
+H = ConvexNormalization(
+    f, state_size=2, key=jr.key(0), epsilon=jnp.array(1e-6)
+)
+J = MatrixWrapper(
+    klax.nn.ConstantSkewSymmetricMatrix(shape=(2, 2), key=jr.key(0)),
+    state_dependent=False,
+    parameter_dependent=False,
+)
+R = MatrixWrapper(
+    klax.nn.SPDMatrix(3, (2, 2), [8], key=jr.key(0)),
+    state_dependent=True,
+    parameter_dependent=True,
+)
+phs = ISOPHS(
+    hamiltonian=H,
+    structure_matrix=J,
+    dissipation_matrix=R,
+)
+model = ODESolver(phs)
+model = klax.finalize(model)
+
+xs = model(
+    ts=jnp.linspace(0.0, 100.0, 1000),
+    x0=jnp.array([1.0, 0.0]),
+    args=jnp.array([-1.0]),
+)
+
+plt.plot(xs[:, 0], xs[:, 1])
+
+# %%
